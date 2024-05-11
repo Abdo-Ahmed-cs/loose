fb9e3e0211feafac2403a16597ca489250799ab0
Abdelrahman Ahmed Saad <abdelrahman9789@gmail.com>
2024-05-11 17:06:04 +0300

    login pagr +1

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -1,7 +1,32 @@
+import AuthForm from '@/components/forms/AuthForm'
+import Image from 'next/image'
 import React from 'react'
 
-export default function pages() {
+export default function page() {
     return (
-        <div>pages</div>
+        <section className='log-in'>
+            <div className='overflow-hidden col-span-3'>
+                <Image
+                    src={"/images/login.png"}
+                    width={842}
+                    height={900}
+                    alt='login image background'
+                    className='!w-full !h-full object-cover'
+                />
+            </div>
+            <div className='flex justify-center px-[80px] py-[50px] col-span-2'>
+                <div className='w-full'>
+                    <Image
+                        src={"/icons/logo.svg"}
+                        width={147}
+                        height={42}
+                        alt='loose logo image'
+                        className='mb-[4.25rem]'
+                    />
+
+                    <AuthForm />
+                </div>
+            </div>
+        </section>
     )
 }
